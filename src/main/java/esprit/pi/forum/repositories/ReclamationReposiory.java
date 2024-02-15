fcb51902a9cf3b925e95783346c867d61bbcf9d4
HaithemAttia <haithem.attia@esprit.tn>
2024-02-15 11:58:28 +0100

    1st commit

---
+++ src/main/java/esprit/pi/forum/repositories/ReclamationReposiory.java
@@ -0,0 +1,7 @@
+package esprit.pi.forum.repositories;
+
+import esprit.pi.forum.entities.Reclamation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReclamationReposiory extends JpaRepository<Reclamation,Long> {
+}
